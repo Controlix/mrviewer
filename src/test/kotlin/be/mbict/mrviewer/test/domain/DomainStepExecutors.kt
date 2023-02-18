bff3d7bc65b4745f83f6a27e0c370e5732a231c2
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2023-02-18 09:53:39 +0100

    Use conditional beans

--- src/test/kotlin/be/mbict/mrviewer/test/domain/DomainStepExecutors.kt
+++ src/test/kotlin/be/mbict/mrviewer/test/domain/DomainStepExecutors.kt
@@ -3,10 +3,11 @@ package be.mbict.mrviewer.test.domain
 import be.mbict.mrviewer.MrRepository
 import be.mbict.mrviewer.test.steps.OutputStepExecutor
 import org.assertj.core.api.Assertions
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
 @Component
-// @Profile("domain")
+@ConditionalOnProperty(value = ["output"], havingValue = "domain")
 class DomainOutputStepExecutor(private val mrRepository: MrRepository): OutputStepExecutor {
 
     override fun checkMrIsFirst(identifier: String) {
--- src/test/kotlin/be/mbict/mrviewer/test/ui/UiStepExecutors.kt
+++ src/test/kotlin/be/mbict/mrviewer/test/ui/UiStepExecutors.kt
@@ -6,19 +6,22 @@ import be.mbict.mrviewer.test.steps.OutputStepExecutor
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.hamcrest.Matchers
 import org.jeasy.random.EasyRandom
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 
-// @Component
+@Component
+@ConditionalOnProperty(value = ["output"], havingValue = "ui")
 class UiOutputStepExecutor(private val mockMvc: MockMvc) : OutputStepExecutor {
 
     override fun checkMrIsFirst(identifier: String) {
         mockMvc.get("/ui")
             .andExpect {
                 status { isOk() }
+                view { name("mr") }
                 content {
                     string(Matchers.containsString("<td><span>MrEvent(objectKind=$identifier"))
                 }
@@ -27,6 +30,7 @@ class UiOutputStepExecutor(private val mockMvc: MockMvc) : OutputStepExecutor {
 }
 
 @Component
+@ConditionalOnProperty(value = ["input"], havingValue = "ui")
 class UiInputStepExecutor(
     private val mockMvc: MockMvc,
     private val mapper: ObjectMapper
